89ee38ff6031dd7463daab4aa51377d7da0c7658
ChetanPandeyGit <85728612+ChetanPandeyGit@users.noreply.github.com>
2021-06-11 16:23:44 +0530

    Add files via upload

---
+++ MCA2B/ChetanPandey_20711100/loop.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Output is : ");
+
+		for(int i = 1; i <= 5; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
+
