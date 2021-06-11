80042fd9c06d11462d09123b63fd567a05d3cd15
srvnegi <85733266+srvnegi@users.noreply.github.com>
2021-06-11 17:14:52 +0530

    Add files via upload

---
+++ MCA2B/saurabh singh negi- 2001136/Integer.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Integer
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Output is : ");
+
+        for(int i = 1; i <= 5; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
