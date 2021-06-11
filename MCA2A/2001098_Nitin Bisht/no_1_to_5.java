0ae515496d88b6006cef0f74954bd79b2e82ad1c
Nitin Bisht <71823929+Soul-Arsenal@users.noreply.github.com>
2021-06-11 16:46:09 +0530

    Create no_1_to_5.java

---
+++ MCA2A/2001098_Nitin Bisht/no_1_to_5.java
@@ -0,0 +1,16 @@
+package IncludeHelp;
+
+public class Print_1_To_10_UsingFor 
+{
+	public static void main(String[] args)
+	{
+		//print the result
+		System.out.println("Output is : ");
+
+		//loop  to print 1 to 10.
+		for(int i = 1; i <= 10; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
