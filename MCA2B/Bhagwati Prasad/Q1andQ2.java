ecc046ed5e47c53ab2c06b0c1369c380e8736fe4
bhagwatiprasad <48825037+bhagwatiprasad@users.noreply.github.com>
2021-06-11 16:16:32 +0530

    Delete Q1andQ2.java

--- MCA2B/Bhagwati Prasad/Q1andQ2.java
+++
@@ -1,8 +0,0 @@
-class Q1andQ2{
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        for(int i =0 ; i < 7; i++ ){
-            System.out.println(i);
-        }
-    }
-}
