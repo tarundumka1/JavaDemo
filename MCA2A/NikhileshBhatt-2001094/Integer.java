7b852f403c4bed85432ae85abcfeef550cce4367
Nikhilesh-Bhatt <85735215+Nikhilesh-Bhatt@users.noreply.github.com>
2021-06-11 16:47:25 +0530

    Add files via upload

---
+++ MCA2A/NikhileshBhatt-2001094/Integer.java
@@ -0,0 +1,14 @@
+class Integer 
+{
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Integers are : ");
+
+		
+		for(int i = 1; i <= 5; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
