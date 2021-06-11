61f322b4f2feb7e853c5b23fbf864993015b6b43
deptiwddn <tdeepak44410@gmail.com>
2021-06-11 16:39:25 +0530

    Print 1 to 5

---
+++ MCA2A/Print1to5.java
@@ -0,0 +1,12 @@
+package TIHLoops;
+
+public class Print1to5 {
+
+	public static void main(String[] args) {
+		for(int number=1; number<=5; number++){
+			System.out.println(number);
+		}
+
+	}
+
+}
